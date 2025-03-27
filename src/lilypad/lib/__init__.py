0c4805adb66a4d29a521d4a831de154819f7c492
Koudai Aono <koudai@mirascope.io>
2025-03-27 10:52:35 +0900

    feat: delete generations.py

--- src/lilypad/lib/__init__.py
+++ src/lilypad/lib/__init__.py
@@ -5,11 +5,9 @@ from .tools import tool
 from .traces import trace
 from .messages import Message
 from ._configure import configure
-from .generations import generation
 
 __all__ = [
     "configure",
-    "generation",
     "Message",
     "span",
     "tool",
