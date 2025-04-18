71999655261ac4086d2a5c26481f1abfd3df4113
Koudai Aono <koudai@mirascope.io>
2025-04-18 12:18:09 +0900

    feat: improve orjson option

--- src/lilypad/lib/_utils/json.py
+++ src/lilypad/lib/_utils/json.py
@@ -5,6 +5,6 @@ import orjson
 
 def json_dumps(obj: Any) -> str:
     """Serialize Python objects to JSON using orjson."""
-    # json should be utf-8 encoded
-    return orjson.dumps(obj).decode("utf-8")
+    # json should be utf-8 encoded, json key should be str
+    return orjson.dumps(obj, option=orjson.OPT_NON_STR_KEYS).decode("utf-8")
 
